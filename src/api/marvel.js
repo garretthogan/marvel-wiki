ffc06bcd59df4609c2e69ab05e5f526e24ec6313
Garrett Hogan <garrett.hogan@nike.com>
2018-04-24 15:30:42 -0700

    refactored

--- src/api/marvel.js
+++ src/api/marvel.js
@@ -4,7 +4,7 @@ require('es6-promise').polyfill();
 const PUBLIC_KEY = process.env.PUBLIC_KEY || require('../.env.json').PUBLIC_KEY;
 const baseURL = 'https://gateway.marvel.com';
 
-export function getCharacters() {
-  return fetch(`${baseURL}/v1/public/characters?apikey=${PUBLIC_KEY}&limit=21`).then(res => res.json())
+export function getCharacters(offset = 0) {
+  return fetch(`${baseURL}/v1/public/characters?apikey=${PUBLIC_KEY}&offset=${offset}&limit=21`).then(res => res.json())
           .then(json => json.data.results);
 }
--- src/components/HeroOverlay.js
+++ src/components/HeroOverlay.js
@@ -5,7 +5,7 @@ export default ({name, description, toggleOpen, isOpen}) => (
   <Overlay isOpen={isOpen} onClose={toggleOpen}>
     <Card className="hero-overlay-card" interactive={true} elevation={Elevation.TWO}>
       <h5 className="hero-name"><a>{name}</a></h5>
-      <p>{description}</p>
+      <p>{description || 'No description...'}</p>
       <Button onClick={toggleOpen}>Exit</Button>
     </Card>
   </Overlay>  
